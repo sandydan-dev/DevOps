a32e1a73bbce022550535851d09bcd349252f291
sandeep <sandydan.dev@gmail.com>
2025-12-03 21:06:05 +0530

    Remove AWS credentials

--- 12-terraform/ec2instance/main.tf
+++ 12-terraform/ec2instance/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "firstInstance" {
-  ami                    = "ami-0fa3fe0fa7920f68e"   # ami amazon linux
+  ami                    = ""   # ami amazon linuxami-0fa3fe0fa7920f68e
   instance_type          = "t2.small"       # instance type
-  key_name               = "k8sKey"      # key-pair name
+  key_name               = ""      # key-pair name k8sKey
   vpc_security_group_ids = ["sg-09df4133c962d11d4"] # sg-09df4133c962d11d4 (mysg) | 08428b044e78337d6 (default)
 
   root_block_device {
